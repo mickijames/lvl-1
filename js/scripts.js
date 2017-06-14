2f138524f06db9594237e36151336cfe9c965e2e
mickijames <micalynjeanette@eagles.ewu.edu>
2017-06-13 23:42:14 -0700

    Added buttons and styling
    * it’s functional
    * [  ] want to try to add click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 // Scripts
-console.log('hello');
+//console.log('hello');
 
 function removeTransition(e) {
-if (e.propertyName !== 'transform') return;
-e.target.classList.remove('playing');
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
 }
 
 function playSound(e) {
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-if (!audio) return;
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-key.classList.add('playing');
-audio.currentTime = 0;
-audio.play();
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function playSoundOnClick(e) {
+    
+    
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+//window.addEventListener('click', playSoundOnClick);
